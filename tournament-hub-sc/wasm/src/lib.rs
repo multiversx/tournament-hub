018e808390218c557d49e1fb73c85be1308d8f4e
Costin Carabaș <costin.carabas@gmail.com>
2025-07-15 12:32:39 +0300

    Compilation updates

--- tournament-hub-sc/wasm/src/lib.rs
+++ tournament-hub-sc/wasm/src/lib.rs
@@ -6,9 +6,9 @@
 
 // Init:                                 1
 // Upgrade:                              1
-// Endpoints:                           11
+// Endpoints:                           12
 // Async Callback (empty):               1
-// Total number of exported functions:  14
+// Total number of exported functions:  15
 
 #![no_std]
 
@@ -31,6 +31,7 @@ multiversx_sc_wasm_adapter::endpoints! {
         getTournament => get_tournament
         getSpectatorBets => get_spectator_bets
         getSpectatorPoolTotal => get_spectator_pool_total
+        getAccumulatedHouseFees => get_accumulated_house_fees
     )
 }
 
